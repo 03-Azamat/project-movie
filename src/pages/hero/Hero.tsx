377dc65cae7f802de4a6167e0444174b26669347
Azamat Nurov <azamat@mancho.dev>
2022-09-24 00:58:37 +0600

    add component

---
+++ src/pages/hero/Hero.tsx
@@ -0,0 +1,10 @@
+import { Box, Container } from "@chakra-ui/react";
+import React from "react";
+
+export default function Hero() {
+  return (
+    <Box>
+      <Container>HERO</Container>
+    </Box>
+  );
+}
